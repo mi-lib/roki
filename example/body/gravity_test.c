1b23ffb1b7607f367ecee87a7d0979123e38ebe3
kdaic <develop@kdaic.net>
2024-12-05 00:54:05 +0900

    Delete abolished macros rkGravitySet and rkGravityReset in gravity_test

--- example/body/gravity_test.c
+++ example/body/gravity_test.c
@@ -6,14 +6,5 @@ int main(void)
   zVec6DPrint( RK_GRAVITY6D );
   zVec3DPrint( RK_GRAVITY3D );
 
-  printf( "+++ clear gravity +++\n" );
-  rkGravitySet( 0 );
-  zVec6DPrint( RK_GRAVITY6D );
-  zVec3DPrint( RK_GRAVITY3D );
-
-  printf( "+++ reset gravity +++\n" );
-  rkGravityReset();
-  zVec6DPrint( RK_GRAVITY6D );
-  zVec3DPrint( RK_GRAVITY3D );
   return 0;
 }
